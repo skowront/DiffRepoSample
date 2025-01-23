d1b5fb12747e5cd736e009935df792bd5bc5c2df
skowront <tomasz.skowron98@gmail.com>
2025-01-23 21:35:15 +0100

    .

--- DiffRepoSample/Program.cs
+++ DiffRepoSample/Program.cs
@@ -6,7 +6,6 @@ namespace DiffRepoSample
         {
             var builder = WebApplication.CreateBuilder(args);
             var app2 = builder.Build();
-            var x23 = 10;
             app2.MapGet("/", () => "Hello World!");
 
             app2.Run();
